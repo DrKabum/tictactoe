f0325d4037dcc878b7ce28b851bee3d0876498ba
Nicolas Broca <n.broca@gmail.com>
2021-03-10 04:13:03 +0100

    Add restart and refactor app.js

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const message = document.getElementById('message')
 let line1 = ['', '', '']
 let line2 = ['', '', '']
 let line3 = ['', '', '']
-const table = [line1, line2, line3]
+let table = [line1, line2, line3]
+let isRunning = true
 let p1turn = true
 
 // grid initialization
 function generateGrid() {
     table.forEach((line, index) => {
-        for(let i = 0; i < line.length; i++) {
+        for (let i = 0; i < line.length; i++) {
             const el = document.createElement('div')
             el.classList.add('tile')
             el.id = `tile-${index}-${i}`
@@ -26,17 +27,18 @@ function generateGrid() {
 
 function playRound(e) {
     const mark = p1turn ? "x" : "o"
-    const {col, row} = e.target.dataset
+    const { col, row } = e.target.dataset
 
-    if(table[row][col] === "" && !isGameOver()) {
+    if (table[row][col] === "" && isRunning) {
         table[row][col] = mark
         e.target.textContent = mark
-        if(isGameOver()) {
+        if (isGameOver()) {
+            isRunning = false
             message.textContent = `${p1turn ? "Player 1" : "Player 2"} has won!`
         } else {
             p1turn = !p1turn
             message.textContent = `${p1turn ? "Player 1" : "Player 2"} turn`
-        }        
+        }
     }
 }
 
@@ -45,17 +47,17 @@ function isGameOver() {
     const diagonal1 = [line1[0], line2[1], line3[2]]
     const diagonal2 = [line1[2], line2[1], line3[0]]
     // line check
-    if(isLineWon(table)) return true
+    if (isLineWon(table)) return true
     // column check
-    else if(isLineWon(rowsAsLines)) return true
+    else if (isLineWon(rowsAsLines)) return true
     // diagonals
-    else if(isSameOnLine(diagonal1) || isSameOnLine(diagonal2)) return true
+    else if (isSameOnLine(diagonal1) || isSameOnLine(diagonal2)) return true
     else return false
 }
 
 function isLineWon(array) {
-    for(const line of array) {
-        if(isSameOnLine(line)) return true
+    for (const line of array) {
+        if (isSameOnLine(line)) return true
     }
 }
 
@@ -63,11 +65,23 @@ function isSameOnLine(array) {
     return array.every(v => v === "x") || array.every(v => v === "o")
 }
 
+function restart() {
+    line1 = ['', '', '']
+    line2 = ['', '', '']
+    line3 = ['', '', '']
+    table = [line1, line2, line3]
+    grid.childNodes.forEach(tile => tile.textContent = '')
+    isRunning = true
+    p1turn = true
+    message.textContent = `${p1turn ? "Player 1" : "Player 2"} turn`
+}
+
 // listeners
 function initializeGridListeners() {
     const tiles = grid.childNodes
-    tiles.forEach(item => item.addEventListener('click', (e) => playRound(e) ))
+    tiles.forEach(item => item.addEventListener('click', (e) => playRound(e)))
 }
+restartBtn.addEventListener('click', restart)
 
 generateGrid()
 initializeGridListeners()
